3d5826d84cb8dd3b72168356c936224a39794c81
Juanse <jsvargasc@unal.edu.co>
2021-01-28 15:47:20 -0500

    Controls in code

--- Aprendiendo/Aprendiendo/ViewController.swift
+++ Aprendiendo/Aprendiendo/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - Referencias UI
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
